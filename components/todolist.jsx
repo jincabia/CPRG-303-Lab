b7f844e7b3c5c07699f57615dfe028b6b68f9a70
jincabia <140627796+jincabia@users.noreply.github.com>
2024-01-26 12:25:22 -0700

    Lab 2 Completed

---
+++ components/todolist.jsx
@@ -0,0 +1,59 @@
+    /**
+ * My To Do List App
+ *
+ * @format
+ */
+
+    import React from 'react';
+    import {
+      SafeAreaView,
+      StyleSheet,
+      Pressable,
+      View,
+      Text,
+      ScrollView      
+    } from 'react-native';
+    
+export default function ToDoList()
+{
+
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <Pressable>
+          <View style={[styles.task, styles.completed]}>
+            <Text style={styles.taskText}>Do laundry</Text>
+          </View>
+        </Pressable>
+        <Pressable>
+          <View style={[styles.task]}>
+            <Text style={styles.taskText}>Go to gym</Text>
+          </View>
+        </Pressable>
+        <Pressable>
+          <View style={[styles.task, styles.completed]}>
+            <Text style={styles.taskText}>Walk dog</Text>
+          </View>
+        </Pressable>
+      </ScrollView>
+      
+      
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  task: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+  },
+  completed: {
+    backgroundColor: '#e0e0e0',
+  },
+  taskText: {
+    fontSize: 16,
+  }
+});
+
+
